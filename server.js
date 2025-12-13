40aab385d9ef634d6e5f5ef602bf8d438913690f
ChaCha99999 <91878616+ChaCha99999@users.noreply.github.com>
2025-12-13 04:03:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,30 +6,48 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const path = require('path');
 
+// --- AKILLI DOSYA BULMA SİSTEMİ ---
+// 1. Önce 'public' klasörüne bak
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 2. Bulamazsan ana klasöre bak
 app.use(express.static(__dirname));
 
+// 3. Ana sayfaya girildiğinde index.html'i zorla
 app.get('/', (req, res) => {
     const fs = require('fs');
+    // Public içinde index.html var mı?
     if (fs.existsSync(path.join(__dirname, 'public', 'index.html'))) {
         res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    } else {
+    } 
+    // Yoksa ana dizinde index.html var mı?
+    else if (fs.existsSync(path.join(__dirname, 'index.html'))) {
         res.sendFile(path.join(__dirname, 'index.html'));
+    }
+    else {
+        res.send('HATA: index.html dosyası bulunamadı!');
     }
 });
 
-// --- İLETİŞİM MERKEZİ ---
+// --- OYUN BAĞLANTILARI ---
 io.on('connection', (socket) => {
-  console.log('Birisi bağlandı!');
+  console.log('Bir oyuncu bağlandı: ' + socket.id);
 
-  // 1. Biri ZAR atarsa:
+  // 1. ZAR ATMA OLAYI
   socket.on('zar_atildi', (veri) => {
-    // Sunucu bunu alır ve HERKESE (io.emit) geri yollar
-    io.emit('herkes_icin_zar', veri); 
+    // Zarı atan kişi zaten sonucunu görüyor.
+    // Bu yüzden "broadcast" kullanarak zarı atan HARİÇ herkese yolluyoruz.
+    socket.broadcast.emit('herkes_icin_zar', veri); 
+  });
+
+  // 2. KARAKTER GÜNCELLEME OLAYI (YENİ)
+  socket.on('party_update', (yeniVeri) => {
+    // Listeyi biri güncellediğinde, diğer herkesin ekranını güncelliyoruz.
+    socket.broadcast.emit('party_update_client', yeniVeri);
   });
 
   socket.on('disconnect', () => {
-    console.log('Birisi ayrıldı.');
+    console.log('Oyuncu ayrıldı');
   });
 });
 
