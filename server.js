baa4cb1e7d9cc1076586d09881cc38537b27d578
ChaCha99999 <91878616+ChaCha99999@users.noreply.github.com>
2025-12-13 15:19:24 +0300

    Implement server memory and map change features
    Added server memory management and map change functionality.

--- server.js
+++ server.js
@@ -13,52 +13,52 @@ app.get('/', (req, res) => {
     const fs = require('fs');
     if (fs.existsSync(path.join(__dirname, 'public', 'index.html'))) {
         res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    } else if (fs.existsSync(path.join(__dirname, 'index.html'))) {
-        res.sendFile(path.join(__dirname, 'index.html'));
     } else {
-        res.send('HATA: index.html bulunamadı!');
+        res.sendFile(path.join(__dirname, 'index.html'));
     }
 });
 
-let voteCounts = {}; // Oyları burada tutacağız
+// --- SUNUCU HAFIZASI (RAM DB) ---
+let serverPartyData = []; // Karakter verileri burada duracak
+let currentMapUrl = null; // Açık olan harita burada duracak
+let voteCounts = {}; 
 
 io.on('connection', (socket) => {
-  // 1. TEMEL FONKSİYONLAR
+  console.log('Bağlantı:', socket.id);
+
+  // 1. YENİ GELENE VERİLERİ YOLLA (Senkronizasyon)
+  socket.emit('party_update_client', serverPartyData);
+  if (currentMapUrl) {
+      socket.emit('map_update_client', currentMapUrl);
+  }
+
+  // 2. TEMEL EYLEMLER
   socket.on('zar_atildi', (veri) => socket.broadcast.emit('herkes_icin_zar', veri));
-  socket.on('party_update', (yeniVeri) => socket.broadcast.emit('party_update_client', yeniVeri));
+  
+  // 3. VERİ GÜNCELLEME (Kalıcı Hafıza Simülasyonu)
+  socket.on('party_update', (yeniVeri) => {
+      serverPartyData = yeniVeri; // Sunucuda güncelle
+      socket.broadcast.emit('party_update_client', serverPartyData); // Diğerlerine yay
+  });
+
+  // 4. HARİTA SİSTEMİ
+  socket.on('map_change', (url) => {
+      currentMapUrl = url;
+      io.emit('map_update_client', url);
+  });
+
+  // 5. OYLAMA & WHISPER & TIMER
   socket.on('timer_start', (s) => io.emit('timer_update', s));
   socket.on('wheel_spin', (d) => io.emit('wheel_result', d));
   socket.on('dm_whisper', (d) => io.emit('receive_whisper', d));
 
-  // 2. OYLAMA SİSTEMİ (MVP)
-  socket.on('start_vote', () => {
-      voteCounts = {}; // Oyları sıfırla
-      io.emit('show_vote_screen'); // Herkese ekranı aç
-  });
-
-  socket.on('cast_vote', (name) => {
-      // Oy ekle
-      if (!voteCounts[name]) voteCounts[name] = 0;
-      voteCounts[name]++;
-  });
-
+  socket.on('start_vote', () => { voteCounts = {}; io.emit('show_vote_screen'); });
+  socket.on('cast_vote', (name) => { if (!voteCounts[name]) voteCounts[name]=0; voteCounts[name]++; });
   socket.on('end_vote', () => {
-      // Sonuçları hesapla
-      let winner = "Kimse";
-      let maxVotes = -1;
-      
-      // En çok oyu alanı bul
-      Object.keys(voteCounts).forEach(candidate => {
-          if (voteCounts[candidate] > maxVotes) {
-              maxVotes = voteCounts[candidate];
-              winner = candidate;
-          }
-      });
-      
-      // Herkese sonucu yolla
-      io.emit('vote_result_announce', {winner: winner, votes: maxVotes});
+      let winner = "Kimse", max = -1;
+      Object.keys(voteCounts).forEach(c => { if(voteCounts[c]>max){max=voteCounts[c]; winner=c;} });
+      io.emit('vote_result_announce', {winner, votes:max});
   });
-
 });
 
 const PORT = process.env.PORT || 3000;
