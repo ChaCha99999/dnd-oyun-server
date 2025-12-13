6eaf876ba7b8889c0142ccdcaef888edaf177d55
ChaCha99999 <91878616+ChaCha99999@users.noreply.github.com>
2025-12-14 01:03:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.get('/', (req, res) => {
     }
 });
 
+// --- BELLEK ---
 let serverPartyData = []; 
-let serverEnemies = [];
+let serverEnemies = []; // Düşmanlar geri geldi
 let currentMapUrl = null; 
 let takenIdentities = []; 
 let dmLogHistory = [];
@@ -34,31 +35,42 @@ function logToDM(type, sender, target, message) {
 }
 
 io.on('connection', (socket) => {
+  // GİRİŞ
   socket.emit('party_update_client', serverPartyData);
-  socket.emit('enemy_update_client', serverEnemies);
+  socket.emit('enemy_update_client', serverEnemies); // Düşmanları gönder
   if (currentMapUrl) socket.emit('map_update_client', currentMapUrl);
   socket.emit('update_taken_identities', takenIdentities);
   socket.emit('dm_log_history_load', dmLogHistory);
 
+  // KİMLİK
   socket.on('claim_identity', (data) => {
       if (data.oldId !== undefined) takenIdentities = takenIdentities.filter(id => id !== data.oldId);
       if (data.newId && !takenIdentities.includes(data.newId)) takenIdentities.push(data.newId);
       io.emit('update_taken_identities', takenIdentities);
   });
 
+  // OYUN
   socket.on('zar_atildi', (veri) => socket.broadcast.emit('herkes_icin_zar', veri));
+  
   socket.on('party_update', (yeniVeri) => {
       serverPartyData = yeniVeri;
       socket.broadcast.emit('party_update_client', serverPartyData);
   });
+
+  // DÜŞMAN GÜNCELLEME (YENİ)
   socket.on('enemy_update', (enemies) => {
       serverEnemies = enemies;
       io.emit('enemy_update_client', serverEnemies);
   });
+
   socket.on('map_change', (url) => { currentMapUrl = url; io.emit('map_update_client', url); });
+
+  // İLETİŞİM
   socket.on('dm_whisper', (d) => { io.emit('receive_whisper', d); logToDM('dm-sent', 'DM', d.targetName, d.message); });
   socket.on('player_whisper', (d) => { io.emit('dm_receive_player_msg', d); logToDM('dm-received', d.sender, 'DM', d.message); });
   socket.on('p2p_whisper', (d) => { io.emit('p2p_whisper_relay', d); logToDM('p2p', d.senderName, d.targetName, d.message); });
+
+  // ARAÇLAR
   socket.on('timer_start', (s) => io.emit('timer_update', s));
   socket.on('wheel_spin', (d) => io.emit('wheel_result', d));
   socket.on('start_vote', () => { io.emit('show_vote_screen'); });
